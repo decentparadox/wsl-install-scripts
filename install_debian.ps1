aaa2c0c960512e2eda23b7940c6854a597d1307b
DecentDox() <84977919+decentparadox@users.noreply.github.com>
2023-01-27 23:11:36 +0530

    Install Scripts

---
+++ install_debian.ps1
@@ -0,0 +1,18 @@
+# Install the Windows Subsystem for Linux feature
+Write-Host "Installing Windows subsystem for Linux"
+Write-Host "Running Kalsar"
+Write-Host "By: Sasank,Arhant,Rishi"
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+# Set the default version of WSL to version 2
+wsl --set-default-version 2
+Write-Host "You selected debian"
+# Install the Ubuntu distribution
+wsl --install -d debian
+
+
+# Restart the computer
+Write-Host "Restarting..."
+Start-Sleep -Seconds 5
+Restart-Computer
---
+++ install_ubuntu.ps1
@@ -0,0 +1,18 @@
+# Install the Windows Subsystem for Linux feature
+Write-Host "Installing Windows subsystem for Linux"
+Write-Host "Running Kalsar"
+Write-Host "By: Sasank,Arhant,Rishi"
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+# Set the default version of WSL to version 2
+wsl --set-default-version 2
+Write-Host "You selected Ubuntu"
+# Install the Ubuntu distribution
+wsl --install -d ubuntu
+
+
+# Restart the computer
+Write-Host "Restarting..."
+Start-Sleep -Seconds 5
+Restart-Computer
